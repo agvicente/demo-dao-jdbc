f72cff391bdcdd1d7a4bd6447e38e7b49fa27eb4
Augusto Custodio <custo@NOT001796.axxiom1.local>
2021-11-05 19:29:04 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,13 +16,22 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===TEST 1: seller findById ===");
-		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println();
 		
 		System.out.println("===TEST 2: seller findByDepartment ===");
 		Department dep = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println();
+		
+		System.out.println("===TEST 3: seller findAll ===");
+		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
